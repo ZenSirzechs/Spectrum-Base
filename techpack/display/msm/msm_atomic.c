e0f91fec6c3b1691f3e4e4b24b02bc372a617edb
Sultan Alsawaf <sultan@kerneltoast.com>
2022-09-01 22:08:54 +0200

    drm/msm: Speed up interrupt processing upon commit
    Since we know an interrupt will be arriving soon when a frame is
    committed, we can anticipate it and prevent the CPU servicing that
    interrupt from entering deep idle states. This reduces display rendering
    latency.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: FraSharp <s23265@iisve.it>

--- techpack/display/msm/msm_atomic.c
+++ techpack/display/msm/msm_atomic.c
@@ -779,6 +779,10 @@ retry:
 
 	WARN_ON(drm_atomic_helper_swap_state(state, false) < 0);
 
+	if (!atomic_cmpxchg_acquire(&priv->pm_req_set, 1, 0))
+		pm_qos_update_request(&priv->pm_irq_req, 100);
+	mod_delayed_work(system_unbound_wq, &priv->pm_unreq_dwork, HZ / 10);
+
 	/*
 	 * Provide the driver a chance to prepare for output fences. This is
 	 * done after the point of no return, but before asynchronous commits
