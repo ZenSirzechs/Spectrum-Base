2b02e2d189abaaa8c80b94b14c03c7575fa638b7
FraSharp <s23265@iisve.it>
2022-09-14 17:45:25 +0200

    media: msm: debug less pt2
    Signed-off-by: FraSharp <s23265@iisve.it>

--- techpack/video/msm/vidc/msm_v4l2_vidc.c
+++ techpack/video/msm/vidc/msm_v4l2_vidc.c
@@ -584,8 +584,10 @@ static int msm_vidc_probe_vidc_device(struct platform_device *pdev)
 	list_add_tail(&core->list, &vidc_driver->cores);
 	mutex_unlock(&vidc_driver->lock);
 
+#ifdef CONFIG_DEBUG_FS
 	core->debugfs_root = msm_vidc_debugfs_init_core(
 		core, vidc_driver->debugfs_root);
+#endif
 
 	vidc_driver->sku_version = core->resources.sku_version;
 
@@ -784,14 +786,18 @@ static int __init msm_vidc_init(void)
 
 	INIT_LIST_HEAD(&vidc_driver->cores);
 	mutex_init(&vidc_driver->lock);
+#ifdef CONFIG_DEBUG_FS
 	vidc_driver->debugfs_root = msm_vidc_debugfs_init_drv();
 	if (!vidc_driver->debugfs_root)
 		d_vpr_e("Failed to create debugfs for msm_vidc\n");
+#endif
 
 	rc = platform_driver_register(&msm_vidc_driver);
 	if (rc) {
 		d_vpr_e("Failed to register platform driver\n");
+#ifdef CONFIG_DEBUG_FS
 		debugfs_remove_recursive(vidc_driver->debugfs_root);
+#endif
 		kfree(vidc_driver);
 		vidc_driver = NULL;
 	}
@@ -802,7 +808,9 @@ static int __init msm_vidc_init(void)
 static void __exit msm_vidc_exit(void)
 {
 	platform_driver_unregister(&msm_vidc_driver);
+#ifdef CONFIG_DEBUG_FS
 	debugfs_remove_recursive(vidc_driver->debugfs_root);
+#endif
 	mutex_destroy(&vidc_driver->lock);
 	kfree(vidc_driver);
 	vidc_driver = NULL;
