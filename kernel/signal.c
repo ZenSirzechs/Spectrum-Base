114b9017c7008d362496bb08627e7a18f41617e3
FraSharp <s23265@iisve.it>
2022-09-15 22:26:57 +0200

    Revert "mm: oom_kill: reap memory of a task that receives SIGKILL"
    This reverts commit 97bf2fb57173bcc73186bcc1b733d944532fbb81.

--- kernel/signal.c
+++ kernel/signal.c
@@ -43,8 +43,6 @@
 #include <linux/compiler.h>
 #include <linux/posix-timers.h>
 #include <linux/livepatch.h>
-#include <linux/oom.h>
-#include <linux/capability.h>
 #include <linux/cgroup.h>
 
 #define CREATE_TRACE_POINTS
@@ -1390,10 +1388,6 @@ int group_send_sig_info(int sig, struct siginfo *info, struct task_struct *p,
 	if (!ret && sig) {
 		check_panic_on_foreground_kill(p);
 		ret = do_send_sig_info(sig, info, p, type);
-		if (capable(CAP_KILL) && sig == SIGKILL) {
-			add_to_oom_reaper(p);
-			ulmk_update_last_kill();
-		}
 	}
 
 	return ret;
