872aad2af068fdb1c8cb42568da01f76c5ed2548
John Galt <johngaltfirstrun@gmail.com>
2022-09-30 11:31:54 +0200

    mm/vmscan: bump swappiness
    Signed-off-by: FraSharp <s23265@iisve.it>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -188,14 +188,11 @@ int kswapd_threads_current = DEF_KSWAPD_THREADS_PER_NODE;
  * From 0 .. 200.  Higher means more swappy.
  */
 int vm_swappiness = 160;
-#else
-int vm_swappiness = 60;
-#endif
 #ifdef CONFIG_OPLUS_MM_HACKS
 /*
- * Direct reclaim swappiness, values range from 0 .. 60. Higher means more swappy.
+ * Direct reclaim swappiness, values range from 0 .. 200. Higher means more swappy.
  */
-int direct_vm_swappiness = 60;
+int direct_vm_swappiness = 160;
 #endif /* CONFIG_OPLUS_MM_HACKS */
 
 /*
